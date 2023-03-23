aebc060df63710ed334f0e7d8de03e496ab48e48
taisegoulart <taise.m.goulart@gmail.com>
2023-03-23 01:49:22 -0300

    Arrumando URI Usando o Rest Client #2

--- servicea/src/main/java/dev/rpmhub/ServiceRemotoB.java
+++ servicea/src/main/java/dev/rpmhub/ServiceRemotoB.java
@@ -8,12 +8,13 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 //Implementando a interface de um cliente para se comunicar com o ServiceB
-@RegisterRestClient(baseUri ="http://localhost:4444/serviceb/") //Porta 4444 foi utilizada pois estamos querendo nos comunicar com o B, mesmo que esse arquivo esteja dentro do Service A
+@RegisterRestClient(baseUri ="http://localhost:4444/serviceb") 
+//Porta 4444 foi utilizada pois estamos querendo nos comunicar com o B, mesmo que esse arquivo esteja dentro do Service A
 
 public interface ServiceRemotoB {
     //Interface fará a conexão com o ServiceB e também estabelece os métodos que devem ser chamados
     @GET
     @Path("/person/{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public Person getPerson (@PathParam("name") String nomePessoa);
+    public Person getPerson (@PathParam("name") String name);
 }
